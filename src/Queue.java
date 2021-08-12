32e83e8aaae2169ed08859fc390c7bbed2b15941
Dexshine <b_o_y_s@hotmail.com>
2021-08-12 16:12:01 +0700

    first build

--- src/Queue.java
+++ src/Queue.java
@@ -1,52 +1,72 @@
-
-
 public class Queue {
-    Node[] arr; // circular Queue
+    Node[] arr; // circular array
     int capacity;
     int front;
     int back;
     int size;
     
+    //constructor
     public Queue(int cap){
-
+        this.arr = new Node[cap];
+        this.capacity = cap;
+        this.size = 0;
+        this.front = 0;
+        this.back = 0;
     }
     
+    //push node at index = back
     public void enqueue(Node node){
-        if (!isFull()){
-            // do something
-            
+        
+        if (!isFull()){//Non-empty queue
+            ++this.size; //increase amount of node in queue
+            this.arr[this.back] = node; //push node at index=back
+            ++this.back;//change index position that we can push next time
+            //circular queue-- if back = capacity,we can't push anymore. we need to set variable back = 0
+            this.back = this.back % this.capacity;
         }else{
             System.out.println("Queue Overflow!!!");
         }
     }
     
+    //pop node at index = front
     public Node dequeue(){
         
-        if (!isEmpty()){
-            // do something
-            
+        if (!isEmpty()){//Non-empty queue
+            --this.size;//decrease amount of node in queue
+            ++this.front;//since we popFront, we need to change index position of new front node
+            return this.arr[this.front - 1]; //return old front node
         }else{
             System.out.println("Queue Underflow!!!");
+            return null;
         }
-        return null; // fix this (out of place)
     }
     
     public boolean isEmpty(){
-        return false; // fix this
+        return this.size == 0;//true if queue is empty
     }
     
     public boolean isFull(){
-        return false; // fix this
+        return this.size == this.capacity; //true if queue is full
     }
     
     public void printCircularIndices(){
-        System.out.println("Front index = " + front + " Back index = " + back);
+        System.out.println("Front index = " + this.front + " Back index = " + this.back);
     }
     
     public void printQueue(){
         if (!isEmpty()){
             System.out.print("[Front] ");
             // do something here
+            //create new variable because this function shouldn't affect attribute of the class
+            int nSize = this.size;
+            int curNode = this.front; //start at front node
+            while(nSize != 0){//print data till size = 0
+                System.out.print(this.arr[curNode].data + " ");
+                ++curNode; //change index to next node
+                //circular queue-- if curNode = capacity, set curNode = 0
+                curNode = curNode % this.capacity;
+                --nSize; //decrease size (while loop)
+            }
             System.out.println("[Back]");
         }else{
             System.out.println("Empty Queue!!!");
--- src/Stack.java
+++ src/Stack.java
@@ -1,39 +1,58 @@
-
 public class Stack {
     Node[] arr; // regular array
     int capacity;
     int size;
 
+    //constructor
     public Stack(int cap){
-        
+        this.arr = new Node[cap];
+        this.capacity = cap;
+        this.size = 0;
     }
     
+    //pushBack node to stack
     public void push(Node node){
-        if (!isFull()){
-            // do something
+        if (!isFull()){//stack isn't full
+            this.arr[size] = node; //push node at index = size
+            //increase amount of node in stack and change index position that we can push next time
+            ++this.size;
         }else{
             System.out.println("Stack Overflow!!!");
         }
     }
+    
+    //popBack node
     public Node pop(){
-        if (!isEmpty()){
-            // do something
+        if (!isEmpty()){//Non-empty stack
+            //decrease amount of node in stack and change index positino that we can push next time
+            --this.size;
+            return this.arr[size];//popBack
         }else{
-            System.out.println("Stack Overflow!!!");
+            System.out.println("Stack Underflow!!!");
+            return null;
         }
-        return null; // fix this (out of place)
     }
     public boolean isFull(){
-        return false; // fix this
+        //if size = cap, return true(array is full)
+        return this.size == this.capacity;
     }
     public boolean isEmpty(){
-        return false; // fix this
+        //if size = 0 return true(array is empty)
+        return this.size == 0;
     }
     
     public void printStack(){
         if (!isEmpty()) {
             System.out.print("[Bottom] ");
             // do something here
+            //create new variable because this functino shouldnt' affect attribute of the class
+            int nSize = this.size;
+            int curNode = 0;//start at index 0
+            while(nSize != 0){//print data till size = 0
+                System.out.print(this.arr[curNode].data + " ");
+                ++curNode;//change index to next node
+                --nSize;//decrease size(for while loop)
+            }
             System.out.println("[Top]");
         } else {
             System.out.println("Empty Stack!!!");
